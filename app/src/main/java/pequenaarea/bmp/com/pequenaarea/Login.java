268da9cbdfb2888dc470ff6593456c16e5b316d2
bremdp <brenomattosp@gmail.com>
2016-08-04 14:03:10 -0300

    Primeira versao

---
+++ app/src/main/java/pequenaarea/bmp/com/pequenaarea/Login.java
@@ -0,0 +1,13 @@
+package pequenaarea.bmp.com.pequenaarea;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
